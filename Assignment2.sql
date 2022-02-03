f2c952ac28dd61d2345c2e397582de48824c65b6
tle66 <57013269+tle66@users.noreply.github.com>
2022-02-03 06:22:46 -0800

    Add files via upload

---
+++ Assignment2.sql
@@ -0,0 +1,66 @@
+SELECT COUNT(*)
+FROM Production.Product
+
+SELECT COUNT(*)
+FROM Production.Product JOIN Production.ProductSubcategory on Product.ProductSubcategoryID = ProductSubcategory.ProductSubcategoryID
+
+
+
+SELECT ProductSubcategoryID, COUNT(Name) AS CountedProducts
+FROM Production.Product
+GROUP BY ProductSubcategoryID
+
+SELECT COUNT(*) AS NoSubCat FROM Production.Product
+WHERE ProductSubcategoryID IS NULL
+
+SELECT ProductID, SUM(quantity) AS TheSum
+FROM Production.ProductInventory
+WHERE LocationID = 40
+GROUP BY ProductID
+HAVING SUM(quantity) < 100
+
+SELECT Shelf, ProductID,SUM(quantity) AS TheSum
+FROM Production.ProductInventory
+WHERE LocationID = 40
+GROUP BY ProductID, shelf
+HAVING SUM(quantity) < 100
+
+SELECT AVG(Quantity) AS TheAvg
+FROM Production.ProductInventory
+Where LocationID = 10
+
+SELECT ProductID, Shelf, AVG(Quantity) AS TheAvg
+FROM Production.ProductInventory
+WHERE LocationID = 10 AND Shelf!='N/A'
+GROUP BY ROLLUP (Shelf, ProductID) 
+ORDER BY Shelf
+
+SELECT Color,Class,COUNT(*) AS TheCount,AVG(ListPrice) AS AvgPrice
+FROM Production.Product
+WHERE Class IS NOT NULL AND Color IS NOT NULL
+GROUP BY GROUPING SETS ((Color), (Class))
+
+
+SELECT c.Name AS Country,s.Name AS Province
+FROM Person.CountryRegion AS c
+JOIN Person.StateProvince AS s ON c.CountryRegionCode = s.CountryRegionCode
+
+
+SELECT c.Name AS Country,s.Name AS Province
+FROM Person.CountryRegion AS c
+JOIN Person.StateProvince AS s ON c.CountryRegionCode = s.CountryRegionCode
+WHERE c.Name IN ('Germany','Canada')
+
+USE Northwind
+GO
+
+
+
+SELECT Count(Products.UnitsOnOrder)
+FROM Products WHERE Products.UnitsOnOrder>0
+
+SELECT TOP 5 SUM(UnitsOnOrder) AS "Saleno", PostalCode 
+FROM Products AS p JOIN Suppliers AS s ON p.SupplierID = s.SupplierID
+GROUP BY PostalCode
+ ORDER BY "Saleno" DESC
+
